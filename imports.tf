a334716d6b1fb5ae88b6c41887db598316e59bcf
Yiling <18350701710@163.com>
2024-10-10 14:50:55 +0800

    Initial commit

---
+++ imports.tf
@@ -0,0 +1,48 @@
+# ##################################################################################
+# # IMPORTS
+# ##################################################################################
+
+# import {
+#   to = module.main.aws_vpc.this[0]
+#   id = "vpc-096a17000aecb9d63" #VPC
+# }
+
+# import {
+#   to = module.main.aws_subnet.public[0]
+#   id = "subnet-0bf35ce6afe82f6f9" #PublicSubnet1
+# }
+
+# import {
+#   to = module.main.aws_subnet.public[1]
+#   id = "subnet-0ea391fdec0be34ad" #PublicSubnet2
+# }
+
+# import {
+#   to = module.main.aws_internet_gateway.this[0]
+#   id = "igw-0bb6eb40db3060719" #InternetGateway
+# }
+
+# import {
+#   to = module.main.aws_route.public_internet_gateway[0]
+#   id = "rtb-0f4df4595156d999d_0.0.0.0/0" #DefaultPublicRoute
+# }
+
+# import {
+#   to = module.main.aws_route_table.public[0]
+#   id = "rtb-0f4df4595156d999d" #PublicRouteTable
+# }
+
+# import {
+#   to = module.main.aws_route_table_association.public[0]
+#   id = "subnet-0bf35ce6afe82f6f9/rtb-0f4df4595156d999d" #PublicSubnet1/PublicRouteTable
+# }
+
+# import {
+#   to = module.main.aws_route_table_association.public[1]
+#   id = "subnet-0ea391fdec0be34ad/rtb-0f4df4595156d999d" #PublicSubnet2/PublicRouteTable
+# }
+
+# import {
+#   to = aws_security_group.ingress
+#   id = "sg-04b7e6742dbe7c025" #NoIngressSecurityGroup
+# }
